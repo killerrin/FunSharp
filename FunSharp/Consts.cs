197cd6579bc06e2b6973fcd5e52e12fed3d271e0
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-03-20 19:09:59 -0400

    Initial Commit

---
+++ FunSharp/Consts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunSharp
+{
+    public static class Consts
+    {
+        public static Randomizer Random = Randomizer.Instance;
+    }
+}
---
+++ FunSharp/NSFW/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FunSharp.NSFW
+{
+    public class Class1
+    {
+    }
+}
